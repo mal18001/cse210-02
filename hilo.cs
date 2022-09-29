a9e4350c8cc0d35e4b5bdfca8ec15f04662c0081
cmaldo16 <cmaldo16@gmail.com>
2022-09-29 16:17:56 -0600

    Added random number

---
+++ hilo.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class card {
+
+    public static void get_new_card() {
+        Random rnd = new Random();
+        int card_number = rnd.Next(1,14);
+    }
+
+
+
+}
