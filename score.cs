2067a0d4a5c8ae0bf21315a3f0a68fabf05c12c8
cmaldo16 <cmaldo16@gmail.com>
2022-10-06 15:18:09 -0600

    added call function to score file

--- score.cs
+++ score.cs
@@ -1,33 +1,34 @@
 namespace HiLo
 {
-   public class score{
-       public int points = 300;
-       public bool correct;
-       public bool no_win_or_lose = true;
+    public class score{
+        public static card call_function = new card();
+        public int points = 300;
+        public bool correct;
+        public bool no_win_or_lose = true;
 
-       public void display_win_or_lose(bool correct, bool no_win_or_lose, int[] card_numbers_used) {
-           if (correct == true) {
-               points += 100 ;
-               Console.WriteLine("\n       You win! Good guess!");
-           }
-           else if (correct == false) {
-               if (no_win_or_lose == true) {
-                   Console.WriteLine("\n Same number! Who would've thought?");
-               }
-               else {
-                   points -= 75;
-                   Console.WriteLine("\n     Sorry, that wasn't right.");
-               }
-           }
-           call_function.loop_until_done(card_numbers_used);
-       }
+        public void display_win_or_lose(bool correct, bool no_win_or_lose, int[] card_numbers_used) {
+            if (correct == true) {
+                points += 100 ;
+                Console.WriteLine("\n       You win! Good guess!");
+            }
+            else if (correct == false) {
+                if (no_win_or_lose == true) {
+                    Console.WriteLine("\n Same number! Who would've thought?");
+                }
+                else {
+                    points -= 75;
+                    Console.WriteLine("\n     Sorry, that wasn't right.");
+                }
+            }
+            call_function.loop_until_done(card_numbers_used);
+        }
 
-       public void gameover(){
-           if (points == 0){
-               Console.WriteLine("Gameover");
-           }
-       }
-   }
+        public void gameover(){
+            if (points == 0){
+                Console.WriteLine("Gameover");
+            }
+        }
+   }    
 
 }
 
